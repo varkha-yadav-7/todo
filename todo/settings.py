77b10650398cee709f2c60cbed33457628441e95
Varkha Yadav <varkhay@gmail.com>
2020-04-30 19:22:19 +0530

    some errors removed

--- todo/settings.py
+++ todo/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+PROJECT_DIR=os.path.dirname(__file__)
+STATIC_ROOT=os.path.join(PROJECT_DIR,'static')
 
 STATIC_URL = '/static/'
